71e0beac9a2580bae1153697076919bbcdc673a5
Niema Moshiri <niemamoshiri@gmail.com>
2022-08-01 14:43:51 -0700

    Initial steps towards conversion

--- GEMF_FAVITES.py
+++ GEMF_FAVITES.py
@@ -7,6 +7,7 @@ Niema Moshiri 2022
 # imports
 from os import chdir, getcwd, makedirs
 from os.path import abspath, expanduser, isdir, isfile
+from subprocess import call
 from sys import argv
 import argparse
 
@@ -14,12 +15,15 @@ import argparse
 C_UINT_MAX = 4294967295
 
 # defaults
+DEFAULT_FN_GEMF_LOG = 'log.txt'
 DEFAULT_FN_GEMF_NETWORK = 'network.txt'
 DEFAULT_FN_GEMF_NODE2NUM = 'node2num.txt'
 DEFAULT_FN_GEMF_OUT = 'output.txt'
 DEFAULT_FN_GEMF_PARA = 'para.txt'
 DEFAULT_FN_GEMF_STATE2NUM = 'state2num.txt'
 DEFAULT_FN_GEMF_STATUS = 'status.txt'
+DEFAULT_FN_TRANSMISSIONS_FAVITES = 'transmission_network.txt'
+DEFAULT_GEMF_PATH = 'GEMF'
 
 def parse_args():
     '''
@@ -36,16 +40,19 @@ def parse_args():
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-c', '--contact_network', required=True, type=str, help="Contact Network (TSV)")
     parser.add_argument('-s', '--initial_states', required=True, type=str, help="Initial States (TSV)")
+    parser.add_argument('-i', '--infected_states', required=True, type=str, help="Infected States (one per line)")
     parser.add_argument('-r', '--rates', required=True, type=str, help="State Transition Rates (TSV)")
     parser.add_argument('-t', '--end_time', required=True, type=float, help="End Time")
     parser.add_argument('-o', '--output', required=True, type=str, help="Output Directory")
     parser.add_argument('--max_events', required=False, type=int, default=C_UINT_MAX, help="Max Number of Events")
-    parser.add_argument('--gemf_path', required=False, type=str, default='GEMF', help="Path to GEMF Executable")
+    parser.add_argument('--rng_seed', required=False, type=int, default=None, help="Random Number Generation Seed")
+    parser.add_argument('--gemf_path', required=False, type=str, default=DEFAULT_GEMF_PATH, help="Path to GEMF Executable")
     args = parser.parse_args()
 
     # convert local paths to absolute
     args.contact_network = abspath(expanduser(args.contact_network))
     args.initial_states = abspath(expanduser(args.initial_states))
+    args.infected_states = abspath(expanduser(args.infected_states))
     args.rates = abspath(expanduser(args.rates))
     args.output = abspath(expanduser(args.output))
     return args
@@ -58,7 +65,7 @@ def check_args(args):
         `args` (`argparse.ArgumentParser`): Parsed user arguments
     '''
     # check that input files exist
-    for fn in [args.contact_network, args.initial_states, args.rates]:
+    for fn in [args.contact_network, args.initial_states, args.infected_states, args.rates]:
         if not isfile(fn):
             raise ValueError("File not found: %s" % fn)
 
@@ -70,7 +77,11 @@ def check_args(args):
     if isdir(args.output) or isfile(args.output):
         raise ValueError("Output directory exists: %s" % args.output)
 
-def prepare_outdir(outdir, para_fn=DEFAULT_FN_GEMF_PARA, network_fn=DEFAULT_FN_GEMF_NETWORK, node2num_fn=DEFAULT_FN_GEMF_NODE2NUM, status_fn=DEFAULT_FN_GEMF_STATUS, state2num_fn=DEFAULT_FN_GEMF_STATE2NUM, out_fn=DEFAULT_FN_GEMF_OUT):
+    # check that RNG seed is non-negative
+    if args.rng_seed is not None and args.rng_seed < 0:
+        raise ValueError("RNG seed must be positive: %d" % args.rng_seed)
+
+def prepare_outdir(outdir, para_fn=DEFAULT_FN_GEMF_PARA, network_fn=DEFAULT_FN_GEMF_NETWORK, node2num_fn=DEFAULT_FN_GEMF_NODE2NUM, status_fn=DEFAULT_FN_GEMF_STATUS, state2num_fn=DEFAULT_FN_GEMF_STATE2NUM, trans_fn=DEFAULT_FN_TRANSMISSIONS_FAVITES):
     '''
     Prepare GEMF output directory
 
@@ -87,7 +98,7 @@ def prepare_outdir(outdir, para_fn=DEFAULT_FN_GEMF_PARA, network_fn=DEFAULT_FN_G
 
         `state2num_fn` (`str`): File name of "state label to GEMF number" mapping file
 
-        `out_fn` (`str`): File name of GEMF output file
+        `trans_fn` (`str`): File name of output FAVITES-format transmission network
 
     Returns:
         `file`: Write-mode file object to GEMF parameter file
@@ -100,7 +111,7 @@ def prepare_outdir(outdir, para_fn=DEFAULT_FN_GEMF_PARA, network_fn=DEFAULT_FN_G
 
         `file`: Write-mode file object to "state label to GEMF number" mapping file
 
-        `file`: Write-mode file object to GEMF output file
+        `file`: Write-mode file object to output FAVITES-format transmission network
     '''
     makedirs(outdir)
     para_f = open('%s/%s' % (outdir, para_fn), 'w')
@@ -108,8 +119,8 @@ def prepare_outdir(outdir, para_fn=DEFAULT_FN_GEMF_PARA, network_fn=DEFAULT_FN_G
     node2num_f = open('%s/%s' % (outdir, node2num_fn), 'w')
     status_f = open('%s/%s' % (outdir, status_fn), 'w')
     state2num_f = open('%s/%s' % (outdir, state2num_fn), 'w')
-    out_f = open('%s/%s' % (outdir, out_fn), 'w')
-    return para_f, network_f, node2num_f, status_f, state2num_f, out_f
+    trans_f = open('%s/%s' % (outdir, trans_fn), 'w')
+    return para_f, network_f, node2num_f, status_f, state2num_f, trans_f
 
 def create_gemf_network(contact_network_fn, network_f, node2num_f):
     '''
@@ -165,7 +176,7 @@ def create_gemf_network(contact_network_fn, network_f, node2num_f):
     node2num_f.write(str(node2num)); node2num_f.write('\n'); node2num_f.close(); network_f.close()
     return node2num, num2node
 
-def create_gemf_status(initial_states_fn, status_f, state2num_f, node2num):
+def create_gemf_status(initial_states_fn, status_f, node2num):
     '''
     Load initial states and convert to GEMF status format
 
@@ -173,8 +184,6 @@ def create_gemf_status(initial_states_fn, status_f, state2num_f, node2num):
         `initial_states_fn` (`str`): Path to initial states file (FAVITES format)
 
         `status_f` (`file`): Write-mode file object to GEMF status file
-
-        `state2num_f` (`file`): Write-mode file object to "state label to GEMF number" mapping file
 
         `node2num` (`dict`): A mapping from node label to node number (for validity checking)
 
@@ -203,16 +212,37 @@ def create_gemf_status(initial_states_fn, status_f, state2num_f, node2num):
     status_f.close()
     return state2num, num2state
 
-def create_gemf_para(rates_fn, para_f, state2num_f, state2num, num2state):
+def create_gemf_para(rates_fn, end_time, max_events, network_fn, status_fn, out_fn, para_f, state2num_f, state2num, num2state, rng_seed=None):
     '''
     Load transition rates and convert to GEMF para format
 
     Args:
-        TODO
+        `rates_fn` (`str`): Path to transition rates file
+
+        `end_time` (`float`): Simulation end time
+
+        `max_events` (`int`): Max number of transition events
+
+        `network_fn` (`str`): File name of GEMF network file
+
+        `status_fn` (`str`): File name of GEMF status file
+
+        `out_fn` (`str`): File name of GEMF output file
+
+        `para_f` (`file`): Write-mode file object to GEMF parameter file
+
+        `state2num_f` (`file`): Write-mode file object to "state label to GEMF number" mapping file
+
+        `state2num` (`dict`): A mapping from state label to state number
+
+        `num2state` (`list`): A mapping from state number to state label
+
+        `rng_seed` (`int`): Seed for random number generation
     '''
     # load transition rates
     RATE = dict() # RATE[by_state][from_state][to_state] = transition rate (by_state == None means nodal transition)
     INDUCERS = set() # state numbers of inducer states
+    matrices = dict() # TODO
     for l in open(rates_fn):
         if len(l) == 0 or l[0] == '#' or l[0] == '\n':
             continue
@@ -262,17 +292,51 @@ def create_gemf_para(rates_fn, para_f, state2num_f, state2num, num2state):
                 rates = ['0']*NUM_STATES
             para_f.write("%s\n" % '\t'.join(rates))
         para_f.write('\n')
-    para_f.write('\n')
+
+    # write remaining sections of parameter file
+    para_f.write("[STATUS_BEGIN]\n0\n\n")
+    para_f.write("[INDUCER_LIST]\n%s\n\n" % ' '.join(str(s) for s in sorted(INDUCERS)))
+    para_f.write("[SIM_ROUNDS]\n1\n\n")
+    para_f.write("[INTERVAL_NUM]\n1\n\n")
+    para_f.write("[MAX_TIME]\n%s\n\n" % end_time)
+    para_f.write("[MAX_EVENTS]\n%d\n\n" % max_events)
+    para_f.write("[DIRECTED]\n1\n\n")
+    para_f.write("[SHOW_INDUCER]\n1\n\n")
+    para_f.write("[DATA_FILE]\n%s\n\n" % network_fn.split('/')[-1])
+    para_f.write("[STATUS_FILE]\n%s\n\n" % status_fn.split('/')[-1])
+    if rng_seed is not None:
+        para_f.write("[RANDOM_SEED]\n%d\n\n" % rng_seed)
+    para_f.write("[OUT_FILE]\n%s\n" % out_fn.split('/')[-1])
+    para_f.close()
+    return matrices
+
+def run_gemf(outdir, log_fn, gemf_path=DEFAULT_GEMF_PATH):
+    '''
+    Run GEMF
+
+    Args:
+        `outdir` (`str`): Path to output directory
+    '''
+    orig_dir = getcwd()
+    chdir(outdir)
+    log_f = open(log_fn, 'w'); call([gemf_path], stdout=log_f); log_f.close()
+    chdir(orig_dir)
+    return log_f
+
+def convert_transmissions_to_favites(matrices, infected_states_fn, out_fn, trans_fn):
+    infected_states = {l.strip() for l in open(infected_states_fn)}
 
 def main():
     '''
     Main function
     '''
     args = parse_args(); check_args(args)
-    para_f, network_f, node2num_f, status_f, state2num_f, out_f = prepare_outdir(args.output)
+    para_f, network_f, node2num_f, status_f, state2num_f, trans_f = prepare_outdir(args.output)
     node2num, num2node = create_gemf_network(args.contact_network, network_f, node2num_f) # closes network_f and node2num_f
-    state2num, num2state = create_gemf_status(args.initial_states, status_f, state2num_f, node2num) # closes status_f
-    create_gemf_para(args.rates, para_f, state2num_f, state2num, num2state) # closes para_f and state2num_f
+    state2num, num2state = create_gemf_status(args.initial_states, status_f, node2num) # closes status_f
+    matrices = create_gemf_para(args.rates, args.end_time, args.max_events, network_f.name, status_f.name, DEFAULT_FN_GEMF_OUT, para_f, state2num_f, state2num, num2state, args.rng_seed) # closes para_f and state2num_f
+    log_f = run_gemf(args.output, DEFAULT_FN_GEMF_LOG, args.gemf_path) # closes log_f
+    convert_transmissions_to_favites(matrices, args.infected_states, '%s/%s' % (args.output, DEFAULT_FN_GEMF_OUT), trans_f)
 
 # execute main function
 if __name__ == "__main__":
