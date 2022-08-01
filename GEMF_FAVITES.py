1f3c5c93b1278b6a6070814f3fe52ceee2caa5cd
Niema Moshiri <niemamoshiri@gmail.com>
2022-08-01 10:00:47 -0700

    Added nodal transition to para file

--- GEMF_FAVITES.py
+++ GEMF_FAVITES.py
@@ -10,6 +10,9 @@ from os.path import abspath, expanduser, isdir, isfile
 from sys import argv
 import argparse
 
+# useful variables
+C_UINT_MAX = 4294967295
+
 # defaults
 DEFAULT_FN_GEMF_NETWORK = 'network.txt'
 DEFAULT_FN_GEMF_NODE2NUM = 'node2num.txt'
@@ -36,6 +39,7 @@ def parse_args():
     parser.add_argument('-r', '--rates', required=True, type=str, help="State Transition Rates (TSV)")
     parser.add_argument('-t', '--end_time', required=True, type=float, help="End Time")
     parser.add_argument('-o', '--output', required=True, type=str, help="Output Directory")
+    parser.add_argument('--max_events', required=False, type=int, default=C_UINT_MAX, help="Max Number of Events")
     parser.add_argument('--gemf_path', required=False, type=str, default='GEMF', help="Path to GEMF Executable")
     args = parser.parse_args()
 
@@ -107,7 +111,7 @@ def prepare_outdir(outdir, para_fn=DEFAULT_FN_GEMF_PARA, network_fn=DEFAULT_FN_G
     out_f = open('%s/%s' % (outdir, out_fn), 'w')
     return para_f, network_f, node2num_f, status_f, state2num_f, out_f
 
-def load_contact_network(contact_network_fn, network_f, node2num_f):
+def create_gemf_network(contact_network_fn, network_f, node2num_f):
     '''
     Load contact network and convert to GEMF network format
 
@@ -143,11 +147,11 @@ def load_contact_network(contact_network_fn, network_f, node2num_f):
                 raise ValueError("Last column of contact network EDGE row must be exactly d or u")
             try:
                 u_num = node2num[u]
-            except:
+            except KeyError:
                 raise ValueError("Node found in EDGE section but not in NODE section: %s" % u)
             try:
                 v_num = node2num[v]
-            except:
+            except KeyError:
                 raise ValueError("Node found in EDGE section but not in NODE section: %s" % v)
             network_f.write('%d\t%d\n' % (u_num, v_num))
             if d_or_u == 'u':
@@ -161,7 +165,7 @@ def load_contact_network(contact_network_fn, network_f, node2num_f):
     node2num_f.write(str(node2num)); node2num_f.close(); network_f.close()
     return node2num, num2node
 
-def load_initial_states(initial_states_fn, status_f, state2num_f, node2num):
+def create_gemf_status(initial_states_fn, status_f, state2num_f, node2num):
     '''
     Load initial states and convert to GEMF status format
 
@@ -187,17 +191,65 @@ def load_initial_states(initial_states_fn, status_f, state2num_f, node2num):
         u, s = l.split('\t'); u = u.strip(); s = s.strip()
         try:
             u_num = node2num[u]
-        except:
+        except KeyError:
             raise ValueError("Encountered node in inital states file that is not in contact network file: %s" % u)
         try:
             s_num = state2num[s]
-        except:
+        except KeyError:
             s_num = len(num2state); state2num[s] = s_num; num2state.append(s)
         status_f.write('%d\n' % s_num)
 
     # finish up and return
-    state2num_f.write(str(state2num)); state2num_f.close(); status_f.close()
+    #state2num_f.write(str(state2num)); state2num_f.close() # TODO MOVE TO create_gemf_para
+    status_f.close()
     return state2num, num2state
+
+def create_gemf_para(rates_fn, para_f, state2num_f, state2num, num2state):
+    '''
+    Load transition rates and convert to GEMF para format
+
+    Args:
+        TODO
+    '''
+    # load transition rates
+    RATE = dict() # RATE[by_state][from_state][to_state] = transition rate (by_state == None means nodal transition)
+    INDUCERS = set() # state numbers of inducer states
+    for l in open(rates_fn):
+        if len(l) == 0 or l[0] == '#' or l[0] == '\n':
+            continue
+        from_s, to_s, by_s, r = l.split('\t'); from_s = from_s.strip(); to_s = to_s.strip(); by_s = by_s.strip(); r = float(r)
+        try:
+            from_s_num = state2num[from_s]
+        except KeyError:
+            from_s_num = len(num2state); state2num[from_s] = from_s_num; num2state.append(from_s)
+        try:
+            to_s_num = state2num[to_s]
+        except KeyError:
+            to_s_num = len(num2state); state2num[to_s] = to_s_num; num2state.append(to_s)
+        if by_s.lower() == 'none':
+            by_s = None; by_s_num = None
+        else:
+            try:
+                by_s_num = state2num[by_s]
+            except KeyError:
+                by_s_num = len(num2state); state2num[by_s] = by_s_num; num2state.append(by_s); INDUCERS.add(by_s_num)
+        if by_s_num not in RATE:
+            RATE[by_s_num] = dict()
+        if from_s_num not in RATE[by_s_num]:
+            RATE[by_s_num][from_s_num] = dict()
+        if to_s_num in RATE[by_s_num][from_s_num]:
+            raise ValueError("Duplicate transition encountered: from '%s' to '%s' by '%s'" % (from_s, to_s, by_s))
+        RATE[by_s_num][from_s_num][to_s_num] = r
+    state2num_f.write(str(state2num)); state2num_f.close(); NUM_STATES = len(state2num)
+
+    # write nodal transition matrix (by_state == None)
+    para_f.write("[NODAL_TRAN_MATRIX]\n")
+    for s in range(NUM_STATES):
+        if s in RATE[None]:
+            rates = [str(RATE[None][s][s_to]) if s_to in RATE[None][s] else '0' for s_to in range(NUM_STATES)]
+        else:
+            rates = ['0']*NUM_STATES
+        para_f.write("%s\n" % '\t'.join(rates))
 
 def main():
     '''
@@ -205,8 +257,9 @@ def main():
     '''
     args = parse_args(); check_args(args)
     para_f, network_f, node2num_f, status_f, state2num_f, out_f = prepare_outdir(args.output)
-    node2num, num2node = load_contact_network(args.contact_network, network_f, node2num_f) # closes network_f and node2num_f
-    state2num, num2state = load_initial_states(args.initial_states, status_f, state2num_f, node2num)
+    node2num, num2node = create_gemf_network(args.contact_network, network_f, node2num_f) # closes network_f and node2num_f
+    state2num, num2state = create_gemf_status(args.initial_states, status_f, state2num_f, node2num) # closes status_f
+    create_gemf_para(args.rates, para_f, state2num_f, state2num, num2state) # closes para_f and state2num_f
 
 # execute main function
 if __name__ == "__main__":
