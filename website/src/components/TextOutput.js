8c031cf76046c5c2d96f6ac69d432d88a15f343e
Daniel Ji <jidaniel1234@gmail.com>
2023-01-26 03:25:34 -0800

    start debugging tool runtime bottleneck

--- website/src/components/TextOutput.js
+++ website/src/components/TextOutput.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react'
 
 export class TextOutput extends Component {
-  render() {
+    getText = () => {
+        if (!this.props.text) {
+            return '';
+        }
+
+        if (this.props.text.length <= 10000) {
+            return this.props.text;
+        }
+
+        return `${this.props.text.substring(0, 10000)} ...\n10,000 characters displayed, ${this.props.text.length - 10000} characters more`;
+    }
+
+
+    render() {
+        const text = this.getText();
+        
         return (
             <div className="output my-3">
                 {this.props.label && 
@@ -9,7 +24,7 @@ export class TextOutput extends Component {
                     <input className="form-check-input my-1" type="checkbox" checked={this.props.downloadFile} id={this.props.id} onChange={this.props.toggleDownloadFile} />
                 </h3>
                 }
-                <textarea id={this.props.id} className="form-control" readOnly value={this.props.text} />
+                <textarea id={this.props.id} className="form-control" readOnly value={text} />
             </div>
         )
     }
